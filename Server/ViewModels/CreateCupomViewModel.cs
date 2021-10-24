ea765061fc4f6b55f8322606a2c4748028dea4f5
andersonr <anderson.rissardi@hotmail.com>
2021-10-24 10:05:26 -0300

    Commitando o código produzido ontem, agora com gitIgnore para evitar o upload dos arquivos incorretos

---
+++ Server/ViewModels/CreateCupomViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.ViewModels
+{
+    public class CreateCupomViewModel
+    {
+        [Required]
+        public string Codigo { get; set; }
+        [Required]
+        public decimal PercentualDesconto { get; set; }
+    }
+}
